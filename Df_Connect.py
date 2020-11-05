77d18264e9b67e72b0eb7f535e7129b3bea31bf9
rockqinlu <32383013@qq.com>
2020-11-05 23:57:43 +0800

    笔记本修改版本

--- Df_Connect.py
+++ Df_Connect.py
@@ -12,22 +12,23 @@ df2 = pd.DataFrame({'A': ['A4', 'A5', 'A6', 'A7'],
                     'D': ['D4', 'D5', 'D6', 'D7']},
                    index=[4, 5, 6, 7])
 df3 = pd.DataFrame({'A': ['A8', 'A9', 'A10', 'A11'],
-                     'B': ['B8', 'B9', 'B10', 'B11'],
-                     'C': ['C8', 'C9', 'C10', 'C11'],
-                     'D': ['D8', 'D9', 'D10', 'D11']},
-                    index=[8, 9, 10, 11])
+                    'B': ['B8', 'B9', 'B10', 'B11'],
+                    'C': ['C8', 'C9', 'C10', 'C11'],
+                    'D': ['D8', 'D9', 'D10', 'D11']},
+                   index=[8, 9, 10, 11])
 df4 = pd.DataFrame({'B': ['B2', 'B3', 'B6', 'B7'],
-                     'D': ['D2', 'D3', 'D6', 'D7'],
-                     'F': ['F2', 'F3', 'F6', 'F7']},
-                    index=[2, 3, 6, 7])
+                    'D': ['D2', 'D3', 'D6', 'D7'],
+                    'F': ['F2', 'F3', 'F6', 'F7']},
+                   index=[2, 3, 6, 7])
 frames = [df1, df2, df3]
-result = pd.concat(frames,keys=['x','y','z'])
+result = pd.concat(frames, keys=['x', 'y', 'z'])
 print('----------------- Begin --------------------')
 print('----------------- 1 --------------------')
 print(result)
 print('----------------- 2 --------------------')
 print(result.loc['y'])
 print('----------------- 3 --------------------')
-result2 = pd.concat([df1,df4],axis=1,join='inner')
+result2 = pd.concat([df1, df4], axis=1, join='inner')
 print(result2)
 print('----------------- 90--------------------')
+print('----------------- 91--------------------')
