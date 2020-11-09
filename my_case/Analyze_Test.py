532fa8d363b565b5d3deaf4c05e502899615f104
rockqinlu <32383013@qq.com>
2020-11-09 23:27:36 +0800

    非现场系统运维数据实践v1

---
+++ my_case/Analyze_Test.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# 1.st : read a .xls file T18_TASK_FACT.xls
+xls_file = pd.ExcelFile('G:\\Temp\\yw\\T18_TASK_FACT.xls')
+df = pd.read_excel(xls_file, 'Sheet1', index_col='序号')
+# print(df['USETIME'].describe())
+# df.to_excel('G:\\Temp\\yw\\path_to_file.xlsx', sheet_name='Sheet1')
+
+plt.figure()
+print(df['USETIME'].plot(kind='bar'))
