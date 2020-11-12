28b0654a917905993ace82819b89bc25b1d64115
rockqinlu <32383013@qq.com>
2020-11-12 21:14:14 +0800

    非现场系统运维数据实践v1

--- my_case/Analyze_Test.py
+++ my_case/Analyze_Test.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from numpy import *
 
 # 1.st : read a .xls file T18_TASK_FACT.xls
 xls_file = pd.ExcelFile('G:\\Temp\\yw\\T18_TASK_FACT.xls')
 df = pd.read_excel(xls_file, 'Sheet1', index_col='序号')
-# print(df['USETIME'].describe())
+print(df['USETIME'].describe())
 # last ： save file
 # df.to_excel('G:\\Temp\\yw\\path_to_file.xlsx', sheet_name='Sheet1')
-
-plt.figure()
-print(df['USETIME'].plot(kind='bar'))
+df['USETIME'].plot.hist()
+# df['USETIME'].plot.density()
+plt.show()
